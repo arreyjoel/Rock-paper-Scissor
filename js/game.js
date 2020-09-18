44d60f265ebd23b4747ec14fc8cacff76e9501ac
Otoniel Reyes Galay <otonielreyesgalay@gmail.com>
2020-09-17 23:40:21 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,79 @@
+const options = "rock,paper,scissors".split(','); // this is the same than=> const options = ["rock", "paper", "scissors"];
 
+function computerPlay(){
+    // let variables are better if you're not going to use it again
+    // using options constant now we don't need to manually asign the chosen one
+    // if use floor is more dificult to reach the upper number
+    let computerChoice = options[Math.round(Math.random()*2)];
+    return computerChoice;
+    // this function do tha same if we do this:
+    // return options[Math.round(Math.random()*2)];
+}
+
+function userPlay(){
+  var userChoise = "";
+  while (true){
+    userChoise = prompt("choose between rock, paper or scissors").toLowerCase();
+    if (userChoise !== "rock" || userChoise !== "paper" || userChoise !== "scissors"){
+      // if it was a good option continue otherwise ask again;
+      break;
+    }
+  }
+  
+  return userChoise;
+}
+
+// to use instead console.log()
+function log(msg){
+  document.getElementById("console").innerHTML = msg;
+}
+
+function play(){
+  let userChoice = userPlay();
+  let computerChoice = computerPlay();
+  
+  if(userChoice === computerChoice){
+    log("It is a tie");
+    return; // this line end the function's execution here, so it save resources
+  }
+    
+  // trying to compare userChoice with rock causes an error, because there isn't a variable rock defined
+  if(userChoice === "rock"){
+    if(computerChoice === "paper"){
+      log("You loose! paper beat rock");
+      return;
+    }
+    else{
+      log("You won! rock beat scissors");
+      return;
+    }
+  }
+  
+  if(userChoice === "paper"){
+    if(computerChoice === "rock"){
+      log("You won! paper beat rock");
+      return;
+    }else{
+      log("You loose! scissors beat paper");
+      return;
+    }
+  }
+  
+  if(userChoice === "scissors"){
+    if(computerChoice === "rock"){
+      log("You loose! rock beat scissors");
+      return;
+    }
+    else{
+      log("You won! scissors beat paper");
+      return;
+    }
+  }
+}
+
+window.onload = function(){
+  // this line will auto run the game when the page load process is done
+  play();
+}
+
+document.getElementById("play-button").addEventListener('click', play, false);
